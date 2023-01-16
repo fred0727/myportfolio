70ee17d2c71bffeb2fc6ac76acd40f62df423531
Freddy Muñoz <freddymh07952023@gmail.com>
2023-01-16 15:15:42 -0500

    Subiendo pagina

---
+++ script.js
@@ -0,0 +1,25 @@
+function mensajeenviado(){
+    Swal.fire({
+        title: 'Gracias!',
+        text: 'Mensaje enviado',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      })
+}
+
+function enviarmail(){
+	/**
+	 * Método que envía un e-mail a través del cliente que tenga por defecto
+	 * @param  {String} insTextoComp Texto que aparecerá dentro del mensaje
+	 */
+	function enviarCorreo(insTextoComp) {
+    var name = document.getElementById('').value
+    var email = document.getElementById('').value
+    var asunto = document.getElementById('').value
+    var mensaje = document.getElementById('').value
+			var sLink = "mailto:" + escape(sEmail)
+			 + "?subject=" + escape("Te han compartido el siguente texto")
+			 + "&body=" + insTextoComp;
+			window.location.href = sLink;
+	}
+}
